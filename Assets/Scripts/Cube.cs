ce2be3b5fd7f702de51cf2e3d5cecf1eab7eb27e
Al3ks3 <40907471+fimiq@users.noreply.github.com>
2026-03-13 11:40:43 +0700

    Complete

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -1,16 +1,57 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Cube : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private const int MaxSplitChance = 100;
+
+    [SerializeField] private float _explotionForce;
+    [SerializeField] private float _explotionRadius;
+
+    private Spawner _spawner;
+    private Transform _transform;
+    private List<Rigidbody>_createdCubes;
+    private Material material;
+
+    private int _splitChance;
+
+    public void Init(Spawner spawner, int chance)
     {
-        
+        _spawner = spawner;
+        _splitChance = chance;
+        material = GetComponent<MeshRenderer>().material;
+        ChangeColor();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ChangeColor()
     {
-        
+        material.color = UnityEngine.Random.ColorHSV();
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        if (TrySplitCube())
+        {
+            _createdCubes = _spawner.FillCubes(transform.position, _splitChance);
+            Explode();
+        }
+
+        Destroy(gameObject);
+    }
+
+    private bool TrySplitCube()
+    {
+        int randomNumber = new System.Random().Next(0, MaxSplitChance+1);
+        return _splitChance >= randomNumber;
+    }
+
+    private void Explode()
+    {
+        foreach(Rigidbody explodableOnjcets in _createdCubes)
+        {
+            explodableOnjcets.AddExplosionForce(_explotionForce, transform.position, _explotionRadius);
+        }
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,16 +1,44 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private Cube _cubePrafab;
+    private int _currentChance = 100;
+    private int _maxCubeCount = 6;
+    private int _minCubeCount = 2;
+
+    private void Start()
     {
-        
+        CreateCube(Vector3.up);
     }
 
-    // Update is called once per frame
-    void Update()
+    public List<Rigidbody> FillCubes(Vector3 position, int chance)
     {
-        
+        List<Rigidbody> createdCubes= new List<Rigidbody>();
+
+        _currentChance = chance/2;
+
+        for(int i = 0; i < GetCubeCount(); ++i)
+        {
+            createdCubes.Add(CreateCube(position).GetComponent<Rigidbody>());
+        }        
+
+        return createdCubes;
     }
+
+    private int GetCubeCount()
+    {
+        return UnityEngine.Random.Range(_minCubeCount, _maxCubeCount + 1);
+    }
+
+    private Cube CreateCube(Vector3 position)
+    {
+        Cube cube = Instantiate(_cubePrafab, position, Quaternion.identity);
+        cube.Init(this, _currentChance);
+
+        return cube;   
+    }
+    
 }
